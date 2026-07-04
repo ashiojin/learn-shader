6dfb78786b27691d49604f12359530695131e348
MORI, Yuji <ashiojin@gmail.com>
2026-07-04 17:13:17 +0900

    Add custom point for custom vertex attribute to be added in future
    See MaterialExtension::specilize() in extended_material.rs

--- sandbox/src/sample/extended_material.rs
+++ sandbox/src/sample/extended_material.rs
@@ -1,10 +1,6 @@
 
 use bevy::{
-    asset::uuid::Uuid,
-    pbr::{ExtendedMaterial, MaterialExtension},
-    prelude::*,
-    render::render_resource::AsBindGroup,
-    shader::ShaderRef,
+    asset::uuid::Uuid, mesh::VertexAttributeDescriptor, pbr::{ExtendedMaterial, MaterialExtension}, prelude::*, render::render_resource::{AsBindGroup, SpecializedMeshPipeline}, shader::ShaderRef,
 };
 
 #[derive(Asset, AsBindGroup, Reflect, Debug, Clone, Default)]
@@ -44,7 +40,59 @@ impl MaterialExtension for MyExtension {
     fn vertex_shader() -> bevy::shader::ShaderRef {
         ShaderRef::Handle(EXTENDED_MATERIAL_WGSL_UUID.into())
     }
+
+    fn specialize(
+        pipeline: &bevy::pbr::MaterialExtensionPipeline,
+        descriptor: &mut bevy::material::descriptor::RenderPipelineDescriptor,
+        layout: &bevy::mesh::MeshVertexBufferLayoutRef,
+        key: bevy::pbr::MaterialExtensionKey<Self>,
+    ) -> std::prelude::v1::Result<(), bevy::material::specialize::SpecializedMeshPipelineError>
+    {
+        assert!(descriptor.vertex.buffers.len() == 1, "Expected only one vertex buffer layout for the mesh");
+
+        // Mesh attributes for Bevy PBR
+        // constructed in `specialize()` of `SpecializedMeshPipeline` for `MeshPipeline`
+        // https://github.com/bevyengine/bevy/blob/c6f634ca9f406d68ba5109d921247b654cb42c10/crates/bevy_pbr/src/render/mesh.rs#L3284
+        let base_mesh_attirbutes_list = [
+            // in specialize()
+            (Mesh::ATTRIBUTE_POSITION, 0),
+            (Mesh::ATTRIBUTE_NORMAL, 1),
+            (Mesh::ATTRIBUTE_UV_0, 2),
+            (Mesh::ATTRIBUTE_UV_1, 3),
+            (Mesh::ATTRIBUTE_TANGENT, 4),
+            (Mesh::ATTRIBUTE_COLOR, 5),
+            // in setup_morph_and_skinning_defs()
+            //   https://github.com/bevyengine/bevy/blob/0eac08ae5da33f39d64ad148740c34c14b38c481/crates/bevy_pbr/src/render/mesh.rs#L3275
+            (Mesh::ATTRIBUTE_JOINT_INDEX, 6),
+            (Mesh::ATTRIBUTE_JOINT_WEIGHT, 7),
+        ];
+
+        let mut vertex_attriutes = Vec::new();
+        // reconstruct the vertex attributes list for the mesh.
+        for (attr, loc) in base_mesh_attirbutes_list {
+            if layout.0.contains(attr) {
+                vertex_attriutes.push(attr.at_shader_location(loc));
+            }
+        }
+
+        // TODO: Here, we add our custom mesh attributes for the extended material.
+        // if layout.0.contains(MY_CUSTOM_MESH_ATTRIBUTE) { vertex_attriutes.push(MY_CUSTOM_MESH_ATTRIBUTE.at_shader_location(8)); }
+
+        let vertex_layout = layout.0.get_layout(&vertex_attriutes)?;
+        descriptor.vertex.buffers = vec![vertex_layout];
+
+        Ok(())
+    }
 }
+fn get_mesh_attr_name_as_idx(idx: usize) -> &'static str {
+    MESH_ATTR_NAMES_AS_IDX.get(idx).copied().unwrap_or("mesh_attr_unknown")
+}
+const MESH_ATTR_NAMES_AS_IDX: [&str; 16] = [
+    "mesh_attr_0", "mesh_attr_1", "mesh_attr_2", "mesh_attr_3",
+    "mesh_attr_4", "mesh_attr_5", "mesh_attr_6", "mesh_attr_7",
+    "mesh_attr_8", "mesh_attr_9", "mesh_attr_10", "mesh_attr_11",
+    "mesh_attr_12", "mesh_attr_13", "mesh_attr_14", "mesh_attr_15",
+];
 
 pub type MyExtendedMaterial = ExtendedMaterial<StandardMaterial, MyExtension>;
 pub type MyExtendedMaterialPlugin = MaterialPlugin<MyExtendedMaterial>;
